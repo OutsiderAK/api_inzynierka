f561fcff0260e18220f3a809c798991a8d2cf9b4
s444394 <adakac7@amu.edu.pl>
2022-06-10 23:54:13 +0200

    repaired viewsets

--- backend_fiszki/fiszki/views.py
+++ backend_fiszki/fiszki/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Fishka
@@ -8,14 +6,12 @@ from rest_framework import status
 from .serializers import FishkaSerializer
 
 
+from rest_framework import viewsets, filters
 
 
-
-<<<<<<< HEAD
 # Create your views here.
-=======
+
 class FishkaApi(APIView):
->>>>>>> 5aedab81e3e3d2d9f070505fc8fbd05fbd7cbe37
 
     def get(self, request):
         data = Fishka.objects.all()
