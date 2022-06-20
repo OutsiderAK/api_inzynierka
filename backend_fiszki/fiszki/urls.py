e2fcac1c1db436e4f47929d3a435cd0615dbd9d9
s444394 <adakac7@amu.edu.pl>
2022-06-20 18:37:07 +0200

    added endpoint for articles

--- backend_fiszki/fiszki/urls.py
+++ backend_fiszki/fiszki/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+
 from fiszki import views
 
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('api/', include(('fiszki.routers', 'fiszki'))),
     path('api/fiszki/', views.FishkaApi.as_view()),
     path('api/fiszki/<int:id>', views.SingleFishkaApi.as_view()),
+    path('api/article/', views.ArticleApi.as_view()),
+    re_path(r'^api/article/(?P<string>[\w\-]+)/$', views.SingleArticleApi.as_view()),
 ]
--- backend_fiszki/fiszki/views.py
+++ backend_fiszki/fiszki/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Fishka
+from .models import Fishka, Article
 from rest_framework import status
-from .serializers import FishkaSerializer
+from .serializers import FishkaSerializer, ArticleSerializer
 from rest_framework import generics
 
 from rest_framework import viewsets, filters
@@ -33,3 +33,25 @@ class SingleFishkaApi(generics.ListAPIView):
         id = self.kwargs['id']
         return Fishka.objects.filter(id=id)
 
+
+class ArticleApi(APIView):
+
+    def get(self, request):
+        data = Article.objects.all()
+        serializer = ArticleSerializer(data, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SingleArticleApi(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs['string']
+        return Article.objects.filter(slug=slug)
